b8e8e95a49269a398aee609b8a5d2554c93b58c5
ajs7777 <abhisaha955@gmail.com>
2025-01-14 08:40:32 +0530

    created a function for upload users data to the firebase firestore

--- Instagram Clone/Core/Authentication/Service/AuthService.swift
+++ Instagram Clone/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import FirebaseAuth
+import Firebase
+import FirebaseFirestore
 
 class AuthService: ObservableObject {
     
@@ -17,19 +19,23 @@ class AuthService: ObservableObject {
         userSession = Auth.auth().currentUser
     }
     
+    @MainActor
     func logIn(email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
             self.userSession = result.user
+            
         } catch {
             print("DEBUG: Error Log in user \(error.localizedDescription)")
         }
     }
     
+    @MainActor
     func createUser(email: String, password: String, username: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
         } catch {
             print("DEBUG: Error creating user \(error.localizedDescription)")
         }
@@ -43,5 +49,11 @@ class AuthService: ObservableObject {
     func logout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+    
+    func uploadUserData(uid : String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedData = try? Firestore.Encoder().encode(user) else { return }
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedData)
     }
 }
