7bdd979dea2bd342cb869f26d1d30519b882c8e2
ajs7777 <abhisaha955@gmail.com>
2025-01-16 22:58:19 +0530

    created a function for uploading the image to the firebase storage

--- Instagram Clone/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Instagram Clone/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -22,20 +22,28 @@ class EditProfileViewModel: ObservableObject {
     @Published var profileImage: Image?
     @Published var fullname = ""
     @Published var bio = ""
+    private var uiImage: UIImage?
     
     init(user: User) {
         self.user = user
     }
     
+    @MainActor
     func loadImage(fromItem item: PhotosPickerItem?) async {
         guard let item = item else { return }
         guard let data =  try? await item.loadTransferable(type: Data.self) else { return }
         guard let uiImage = UIImage(data: data) else { return }
+        self.uiImage = uiImage
         self.profileImage = Image(uiImage: uiImage)
     }
 
     func updateUserData() async throws {
         var data = [String: Any]()
+        
+        if let uiImage = uiImage {
+            let imageUrl = try? await ImageUploader.uploadImage(image: uiImage)
+            data["profileImageUrl"] = imageUrl
+        }
         
         if !fullname.isEmpty && user.fullname != fullname {
             data["fullname"] = fullname
